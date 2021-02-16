3e8a70b01413ffd52fe4ecac24971c995e3ee304
Oleg Shulga <oleg021086@gmail.com>
2021-02-16 21:33:00 +0300

    Fist commit

---
+++ src/test/java/ru/netology/Statistic/StatisticServiceTest.java
@@ -0,0 +1,20 @@
+package ru.netology.Statistic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatisticServiceTest {
+
+    @Test
+    void findMax() {
+        StatisticService service = new StatisticService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+}
